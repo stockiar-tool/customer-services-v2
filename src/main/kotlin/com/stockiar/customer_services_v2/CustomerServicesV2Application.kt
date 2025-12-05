499e03bfd02862463d6565e6296dc23fadd8651f
ianklebold <ianstgo@gmail.com>
2025-12-04 21:09:42 -0300

    First commit

---
+++ src/main/kotlin/com/stockiar/customer_services_v2/CustomerServicesV2Application.kt
@@ -0,0 +1,11 @@
+package com.stockiar.customer_services_v2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerServicesV2Application
+
+fun main(args: Array<String>) {
+	runApplication<CustomerServicesV2Application>(*args)
+}
---
+++ src/test/kotlin/com/stockiar/customer_services_v2/CustomerServicesV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stockiar.customer_services_v2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomerServicesV2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
